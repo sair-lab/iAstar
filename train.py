530bf577d7238539f088cd9df66d5c9f639207a6
XyC0212 <cxy980212@163.com>
2024-03-16 22:38:27 +0800

    basic

---
+++ train.py
@@ -0,0 +1,29 @@
+import os
+import torch
+import argparse
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+class EncoderTrainer():
+
+    def __init__(self) -> None:
+        
+    def train(self):
+        
+
+    def load_config(self):
+        with open(os.path)
+
+    def prepare_data(self):
+
+    def train_epoch(self, epoch):
+        loss_sum = 0.0
+
+    def cal_loss(self):
+        
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
